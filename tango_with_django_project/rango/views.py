6f06d1a3a267cf03604cc7859f74f409223528e6
skregas <skregas@hotmail.com>
2016-06-20 21:03:58 -0400

    Add static and media directories
    Add templates; change views to use them

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    content = "Rango says hey there world!"
-    content += '<br /><a href="about/">About</a>'
-    return HttpResponse(content)
+     # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    content = "Rango says hey, this is the About page"
-    content += '<br /><a href="/rango/">Index</a>'
-    return HttpResponse(content)
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'rango/about.html', context_dict)
+    
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.conf.global_settings import MEDIA_URL, MEDIA_ROOT
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -53,10 +54,11 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_PATH, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,5 +120,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+# Static media server settings
+MEDIA_URL = '/media/'
+# Absolute path to the media directoryS
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
